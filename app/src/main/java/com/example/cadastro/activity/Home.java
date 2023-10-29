5f1fbcc478d71aa89b2b4c482792683e9e8f3f87
millyvasc <camillejaninecte@gmail.com>
2023-10-28 22:53:22 -0300

    Dispositivos Móveis - Persistência de dados no cadastro de usuário.

---
+++ app/src/main/java/com/example/cadastro/activity/Home.java
@@ -0,0 +1,13 @@
+package com.example.cadastro.activity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.cadastro.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/cadastro/helper/DBHelper.java
@@ -0,0 +1,21 @@
+package com.example.cadastro.helper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "appCadastro.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { // Criado a tabela User -> executado apenas umas vez
+        String sql = "CREATE TABLE user (nome TEXT, email TEXT PRIMARY KEY, senha TEXT, telefone TEXT);";
+        db.execSQL(sql);
+    }
+
+    @Override // Para updates
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
